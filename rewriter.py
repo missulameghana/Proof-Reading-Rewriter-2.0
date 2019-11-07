3ad0094bb6efd84a4d6370dc479dd84507b4ca4a
missulameghana <meghanab302@gmail.com>
2019-11-07 10:21:44 +0530

    second message

--- rewriter.py
+++ rewriter.py
@@ -1,13 +1,9 @@
 from nltk.corpus import wordnet
 def syno(word):
 	synonyms = []
-	antonyms = []
 	for syn in wordnet.synsets(word):
 		for l in syn.lemmas():
-			synonyms.append(l.name())
-			if l.antonyms():
-				 antonyms.append(l.antonyms()[0].name())
-
+			synonyms.append(l.name())			
 
 	return set(synonyms)
 	
