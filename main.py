577328644af8fe33c05aca239934d915f6b904e5
missulameghana <meghanab302@gmail.com>
2019-11-07 13:41:09 +0530

    Updated -meghana

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from rewriter import syno
 import re
+import nltk
 string = input("Enter some text: ")
-no_specials_string = re.sub('[!#?,.:";]', '', string)
-words = no_specials_string.split()
-#print(words)
-for word in words:
-	syno_list = syno(word)
-	#print(syno_list)
+no_specials_string = re.sub('[!#?,:";]', '', string)
+sentences = no_specials_string.split('.')
+words = []
+tags = ['NN','NNS','JJ','JJR','JJS','FW','RB','RBR','RBS']
+tags_v = ['VB','VBD','VBG','VBN','VBP','VBZ']
+helping_verbs=['am', 'is', 'are', 'was', 'were', 'be', 'being','been',
+			'will', 'would', 'shall', 'should', 'may', 'might', 'must', 'can','could',
+			'do', 'does','did','have','having','has','had']
+for sentence in sentences:
+    for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
+    	word_lower = word.lower()
+    	if pos in tags:
+        	words.append(word)
+    	elif pos in tags_v:
+    		if word_lower not in helping_verbs:
+    			words.append(word)
+
+for word in list(dict.fromkeys(words)):
+	syno_list = syno(word)	
 	if syno_list:
 		print("Suggested list of synonyms for "+word+" "+str(syno_list)) 
--- rewriter.py
+++ rewriter.py
@@ -1,9 +1,23 @@
 from nltk.corpus import wordnet
+from iitb_dict import iitb_dict
 def syno(word):
-	synonyms = []
-	for syn in wordnet.synsets(word):
-		for l in syn.lemmas():
-			synonyms.append(l.name())			
+	word=word.lower()
+	if word in iitb_dict.keys():
+		return {iitb_dict[word]}
+	else:
+		synonyms = []
+		#ib = wordnet.synset(wordnet.synsets(word)[0].name())
+		#print(ib)
+		for syn in wordnet.synsets(word):
+			#print(syn)
+			for l in syn.lemmas():
+				synonyms.append(l.name())
+				# if word != l.name():
+				# 	cb = wordnet.synset(wordnet.synsets(l.name())[0].name())
+				# 	print(l.name()+" "+str(ib.wup_similarity(cb)))	
 
-	return set(synonyms)
+						
+
+		return set(synonyms)
+# print(syno("active"))
 	
