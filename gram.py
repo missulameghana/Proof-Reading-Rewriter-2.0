9487878964369aec42203655fa8a56ce0df458de
Khushik22 <khushik@iitb.ac.in>
2019-11-23 23:26:17 +0530

    Starting grammar Check- Ritika

---
+++ gram.py
@@ -0,0 +1,7 @@
+import nltk
+from pattern.en import tag,suggest,ngrams,parse,parstree,pprint
+def joinlist(l):
+	return '/'.join(l)
+whfam=["What","When","Where","Who","Whom","Which","Whose","Why","How"]
+
+var=input()
