d5a11909d80408610f52c6b0f4c823f8bbc72cea
Ritika0991 <ritikagoyal0991@gmail.com>
2019-11-25 09:42:52 +0530

    Driver

--- driver.py
+++ driver.py
@@ -8,15 +8,21 @@ words=words[:last]
 i=-1
 for word in words:
 	i=i+1
+	# print(" ".join(words))
 	l=correction(word.lower())
 	if len(l)!=0:
 		print(" ".join(words))
 		print("Suggest list of correct spellings for "+word+" : ",l)
-		option=input("Choose one or to ignore type -1 ")
-		if option!=-1:
-			if option<len(l):
+		option=input("Choose any option or to ignore type -1 ")
+		if int(option)!=-1:
+			if int(option)<len(l):
 				# x=dic[word][option+1]
-				words[i]=l[option+1]
-
+				words[i]=l[int(option)-1]
+print(" ".join(words))
+i=-1
+for word in words:
+	i=i+1
+	x=
+	l=rewriter()
 
 
